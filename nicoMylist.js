ea5263c36aaf9f4d73ba2c2d67a33ff0677397f4
uzuky <uzuky04@gmail.com>
2014-04-02 20:29:34 +0900

    suma-tofonnnitaiou

--- nicoMylist.js
+++ nicoMylist.js
@@ -6,16 +6,19 @@ javascript: (function () {
 		PB = D.getElementById("playingBox"),
 		ML = PB.getElementsByClassName("movieLink");
 		NOA = ML[0].innerHTML;
-		open(NOA.replace(/.*?([a-zA-Z]{0,2}[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
-	} else if (H.match(/blomaga\/ar[0-9]+/)) {
-		open(H.replace(/.*?(ar[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/blomaga/$1'), '_blank', 'width=500,height=360')
-	} else if (H.match(/watch\/bk[0-9]+/)) {
-		open(H.replace(/.*?(bk[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/book/$1'), '_blank', 'width=500,height=360')
-	} else if (H.match(/(www|i|sp)\.nicovideo\.jp\/watch\/[0-9]+/)) {
+		window.open(NOA.replace(/.*?([a-zA-Z]{0,2}\d+).*/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
+	} else if (H.match(/blomaga\/ar\d+/)) {
+		window.open(H.replace(/.*?(ar\d+).*/, 'http://www.nicovideo.jp/mylist_add/blomaga/$1'), '_blank', 'width=500,height=360')
+	} else if (H.match(/watch\/bk\d+/)) {
+		window.open(H.replace(/.*?(bk\d+).*/, 'http://www.nicovideo.jp/mylist_add/book/$1'), '_blank', 'width=500,height=360')
+	} else if (H.match(/(www|i)\.nicovideo\.jp\/watch\/\d+/)) {
 		AD = D.getElementById("watchAPIDataContainer");
-		open(AD.innerHTML.replace(/.+videoId.+?([a-zA-Z]{0,2}[0-9]{1,8}).+/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
-	} else if (H.match(/(www|i|sp)\.nicovideo\.jp\/watch\/..[0-9]+/)) {
-		open(H.replace(/.*?([a-zA-Z]{0,2}[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
+		window.open(AD.innerHTML.replace(/.+videoId.+?([a-zA-Z]{0,2}\d{1,8}).+/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
+	} else if (H.match(/sp\.nicovideo\.jp\/watch\/\d+/)) {
+		DC = D.getElementById("jsDataContainer");
+		window.open(DC.outerHTML.replace(/.+data-video_id.+?([a-zA-Z]{0,2}\d{1,8}).+/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank')
+	} else if (H.match(/(www|i|sp)\.nicovideo\.jp\/watch\/..\d+/)) {
+		window.open(H.replace(/.*?([a-zA-Z]{0,2}\d+).*/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
 	} else if (H.match(/www\.nicovideo\.jp\/(?:mylist\/\d+|my\/mylist(?:\/#\/\d+)?|(?:user\/\d+|my)\/video)/)) {
 		if (H.match(/mylist\/\d+/)) {
 			CNm = 'SYS_box_item';
@@ -34,17 +37,17 @@ javascript: (function () {
 		//alert(scrH);
 		//alert(wSize); muri
 		for (i = 0; i < b.length; i++) {
-			c[i] = b[i].innerHTML.replace(/href="watch\/(..[0-9]+)(?:\?.+)?"(>(?:\n\s+)?<img.+?\n.+?(?:vinfo_length"><span>|videoTime">))/, 'onclick="open(\'/mylist_add/video/$1\', \'_blank\', \'width=500,height=360\'); return false;" href="/mylist_add/video/$1"$2+');
+			c[i] = b[i].innerHTML.replace(/href="watch\/(..\d+)(?:\?.+)?"(>(?:\n\s+)?<img.+?\n.+?(?:vinfo_length"><span>|videoTime">))/, 'onclick="window.open(\'/mylist_add/video/$1\', \'_blank\', \'width=500,height=360\'); return false;" href="/mylist_add/video/$1"$2+');
 			b[i].innerHTML = c[i];
 		}
 	} else {
 		var t = prompt('動画、ブロマガ、書籍のID/URLを入力してください', '');
-		if (t.match(/ar[0-9]+/)) {
-			open(t.replace(/.*?(ar[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/blomaga/$1'), '_blank', 'width=500,height=360')
-		} else if (t.match(/bk[0-9]+/)) {
-			open(t.replace(/.*?(bk[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/book/$1'), '_blank', 'width=500,height=360')
-		} else if (t.match(/.*[0-9]+/)) {
-			open(t.replace(/.*?([a-zA-Z]{0,2}[0-9]+).*/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
+		if (t.match(/ar\d+/)) {
+			window.open(t.replace(/.*?(ar\d+).*/, 'http://www.nicovideo.jp/mylist_add/blomaga/$1'), '_blank', 'width=500,height=360')
+		} else if (t.match(/bk\d+/)) {
+			window.open(t.replace(/.*?(bk\d+).*/, 'http://www.nicovideo.jp/mylist_add/book/$1'), '_blank', 'width=500,height=360')
+		} else if (t.match(/.*\d+/)) {
+			window.open(t.replace(/.*?([a-zA-Z]{0,2}\d+).*/, 'http://www.nicovideo.jp/mylist_add/video/$1'), '_blank', 'width=500,height=360')
 		} else {
 			window.alert('ニコニコ動画用です。何かあったら\nhttps://twitter.com/uzuky \nhttp://nico.ms/ar201595 \nまで\n　v20140402');
 		}
