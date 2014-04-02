a0f26d7f4eb1c2c4f53b39bac1874ac791096aa1
uzuky <uzuky04@gmail.com>
2014-04-02 21:49:35 +0900

    Update nicoMylist.js

--- nicoMylist.js
+++ nicoMylist.js
@@ -36,6 +36,7 @@ javascript: (function () {
 		//alert(scrT);
 		//alert(scrH);
 		//alert(wSize); muri
+		alert(b.[0].innerHTML);
 		for (i = 0; i < b.length; i++) {
 			c[i] = b[i].innerHTML.replace(/href="watch\/(..\d+)(?:\?.+)?"(>(?:\n\s*)?<img.+?alt="").+?("http:\/\/.+?smilevideo\.jp\/smile\?i=\d+").+?(>(?:\n\s*)?(.+?:vinfo_length"><span>|videoTime">))/, 'href="/mylist_add/video/$1" onclick="window.open(\'/mylist_add/video/$1\', \'_blank\', \'width=500,height=360\'); return false;"$2 src=$3$4+');
 			b[i].innerHTML = c[i];
